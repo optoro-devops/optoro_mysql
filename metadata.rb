db3313c287c3d30495db69bc8a740a6a87e3b666
Alex Freidah <afreidah@gmail.com>
2015-02-25 15:06:08 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer 'devops@optoro.com'
 license 'MIT'
 description 'This is a skeleton'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.6'
+version '0.0.7'
