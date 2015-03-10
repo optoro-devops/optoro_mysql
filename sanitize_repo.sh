dea53b268daba51d24962afad55031861bb680de
Steve McGrane <smcgrane@optoro.com>
2015-03-10 10:30:37 -0500

    Updated sanitize script for mac

--- sanitize_repo.sh
+++ sanitize_repo.sh
@@ -4,10 +4,10 @@
 # This assumes the current directory is named something like optoro_*
 # as per cookbook convention.
 DIRNAME=${PWD##*/}
-for i in $(grep -rl optoro_skel | grep -v .git | grep -v ${0##*/} ); do
+for i in $(grep -rl optoro_skel * | grep -v .git | grep -v ${0##*/} ); do
   echo "Replacing optoro_skel in ${i}"
-  sed -i s/optoro_skel/$DIRNAME/g $i
+  sed -i -e "s/optoro_skel/$DIRNAME/g" "$i"
 done
-
+    
 # Remove this file from the new repo
 git rm -f $0
