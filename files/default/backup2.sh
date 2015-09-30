0ddf9e052bd619ecb4adb5bfd04ee860d99c50c9
Alex Freidah <afreidah@optoro.com>
2015-09-30 11:37:58 -0400

    updated check if current mysql is slave or not.  Right now in OT both think they are slaves and are going to run backups AHHHH

--- files/default/backup2.sh
+++ files/default/backup2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Are we running on slave?
-OUTPUT=$(mysql -u root -pmaster -e "show slave status\G" | awk -F":" '/Slave_SQL_Running/ { print $2 }' | tr -d ' ')
+OUTPUT=$(mysql -u root -pmaster -e "select count(1) from information_schema.global_status WHERE variable_name = 'Slave_running' AND variable_value = 'ON';" | head -2 | tail -1``)
 
 if [ -n "$OUTPUT" ]; then #get a syntax error when $OUTPUT is empty
-  if [ $OUTPUT == "Yes" ]; then  #Back it up!
+  if [ $OUTPUT == "1" ]; then  #Back it up!
     /opt/chef/embedded/bin/backup perform --trigger rotation --root-path /var/optoro/backup
   fi
 fi
