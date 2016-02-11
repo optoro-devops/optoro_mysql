f5ad3498c3aa0e9a6a1738b16ce9368f595c6877
Charles Okara <cgokara@gmail.com>
2016-02-11 08:23:21 -0500

    OT-7722 WIP

---
+++ recipes/lean_backup.rb
@@ -0,0 +1,38 @@
+# <
+# This recipe creates a trimmed down dump of the database
+# >
+
+backup_dir = node['optoro_mysql']['backup_directory']
+
+directory "#{backup_dir}/lean" do
+  action :create
+  user 'deploy'
+  group 'deploy'
+  recursive true
+  user 'deploy'
+  mode 0775
+end
+
+file "#{backup_dir}/lean/inv_tables.csv" do
+  owner 'deploy'
+  group 'deploy'
+  mode 0775
+end
+
+template "#{backup_dir}/lean/lean_dump.rb" do
+  source 'lean_dump.rb.erb'
+  owner 'deploy'
+  group 'deploy'
+  mode 0644
+end
+
+cron 'lean-db-dump-cron-job' do
+  minute '0'
+  hour '7'
+  day '*'
+  month '*'
+  weekday '*'
+  user 'deploy'
+  mailto 'devops@optoro.com'
+  command "ruby #{backup_dir}/lean/lean_dump.rb"
+end
